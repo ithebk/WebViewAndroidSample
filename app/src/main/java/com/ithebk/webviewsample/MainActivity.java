e0b4eb0efad4432fe6f000a8f625d8dce63bfdae
Bharath Kumar <bharath.kumar@autoninja.in>
2019-07-26 16:35:10 +0530

    Initial commit

---
+++ app/src/main/java/com/ithebk/webviewsample/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ithebk.webviewsample;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.web_view);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("http://facebook.com");
+    }
+}
